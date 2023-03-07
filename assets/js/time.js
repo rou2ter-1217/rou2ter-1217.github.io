f11a25e8aafde98ed8ef4c6087a500734a8dd97c
rou2ter <remaker1802@outlook.com>
2023-03-07 23:29:39 +0700

    change templates and assets

---
+++ assets/js/time.js
@@ -0,0 +1,3 @@
+var datetime = new Date().getHours()+1;
+console.log(datetime); // it will represent date in the console of developers tool
+document.getElementById("time").textContent = datetime; // represent on webbrowser
