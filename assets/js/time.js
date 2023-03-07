937b26bb555f1d8f479615346b3f837bbc378008
rou2ter <remaker1802@outlook.com>
2023-03-07 23:38:06 +0700

    test

--- assets/js/time.js
+++ assets/js/time.js
@@ -1,3 +1,28 @@
-var datetime = new Date().getHours()+1;
-console.log(datetime); // it will represent date in the console of developers tool
-document.getElementById("time").textContent = datetime; // represent on webbrowser
+function refreshTime() {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1; // Months start at 0!
+    let dd = today.getDate();
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+    
+    const formattedToday = dd + '/' + mm + '/' + yyyy;
+    const timeDisplay = document.getElementById("time");
+    const dateString = new Date().toLocaleString('en-GB',{hour12: false,});
+    const formattedString = dateString.replace(", ", " - ");
+    timeDisplay.textContent = formattedString;
+}
+setInterval(refreshTime, 1000);
+
+
+// const today = new Date();
+// const yyyy = today.getFullYear();
+// let mm = today.getMonth() + 1; // Months start at 0!
+// let dd = today.getDate();
+
+// if (dd < 10) dd = '0' + dd;
+// if (mm < 10) mm = '0' + mm;
+
+// const formattedToday = dd + '/' + mm + '/' + yyyy;
+
+// document.getElementById('DATE').value = formattedToday;
